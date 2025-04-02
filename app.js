a467db1e50f8f814c1f5b5cbef4216de1b5d9a17
Lucas Pivovar <138077530+LucasPivovar@users.noreply.github.com>
2025-04-02 16:36:38 -0300

    atualizaçoes no interatividade

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = new Vue({
     data: {
         produtos: [],
         produto: false,
+        carrinho: [],
+        mensagemAlerta: "item adicionado ao carrinho",
+        alertaAtivo: false,
+        carrinhoAtivo: false,
+
     },
 
     filters: {
@@ -10,6 +15,19 @@ const app = new Vue({
             return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
         }
     },
+
+    computed: {
+        carrinhoTotal() {
+            let total = 0;
+            if (this.carrinho.length > 0){
+                this.carrinho.forEach(item => {
+                    total += item.preco;
+                });
+            }
+            return total;
+        }       
+    },
+
     methods: {
         fetchProdutos() {
             fetch('api/produtos.json')
@@ -27,26 +45,81 @@ const app = new Vue({
                 })
         },
 
+        abrirModal(id){
+            this.fetchProduto(id);
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });  
+        },
+
         fechar_produto({target, currentTarget}){
             if (target === currentTarget){
                 this.produto = false;
             }
         },
 
-        abrirModal(id){
-            this.fetchProduto(id);
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-            
+        clickForaCarrinho({target, currentTarget}){
+            if (target === currentTarget){
+                this.carrinhoAtivo = false;
+            }
+        },
+
+        adicionarItem(){
+            this.produto.estoque--;
+            const {id, nome, preco} = this.produto;
+            this.carrinho.push({ id, nome, preco});
+            this.alerta(`${nome} adicionado ao carrinho`);
+        },
+
+        removerItem(index){
+            this.carrinho.splice(index, 1)
+        },
+
+        checarLocalStorage(){
+            if(window.localStorage.carrinho){
+                this.carrinho = JSON.parse(window.localStorage.carrinho);
+            }
+        },
+
+        compararEstoque(){
+            const items = this.carrinho.filter(({id}) => id === this.produto.id);
+            this.produto.estoque -= items.length;
+        },
+
+        alerta(mensagem){
+            this.mensagemAlerta = mensagem;
+            this.alertaAtivo = true;
+            setTimeout(() => {
+                this.alertaAtivo = false;
+            }, 3000);
+        },
+
+        router(){
+            const hash = document.location.hash;
+            if (hash){
+                this.fetchProduto(hash.replace("#", ""));
+            }
         }
     },  
 
-
+    watch: {
+        produto(){
+            document.title = this.produto.nome || "Techno";
+            const hash = this.produto.id || ""; 
+            history.pushState(null, null, `#${hash}`);
+            if(this.produto){
+                this.compararEstoque();
+            }
+        },
+        carrinho(){
+            window.localStorage.carrinho = JSON.stringify(this.carrinho);
+        }
+    },   
 
     created() {
         this.fetchProdutos();
-    },
-
+        this.router();
+        this.checarLocalStorage();
+    }
 });
